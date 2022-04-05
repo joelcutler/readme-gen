60a84a062d8b1ee33f28c607607d8b31b7158e0d
Joel C <joel7921111@yahoo.com>
2022-04-04 18:57:34 -0600

    writes readme.md but can't get readmeInfo array to genMD.js?

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,3 +1,5 @@
+// const index = require('../index');
+
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {}
@@ -11,8 +13,8 @@ function renderLicenseLink(license) {}
 function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+const generateMarkdown = (readmeInfo) => {
+  return `# ${readmeInfo}
 
 `;
 }
